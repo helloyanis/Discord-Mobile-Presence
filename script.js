434b4ac452c86f1cbf3d6008b3e4987e4c8a011d
helloyanis <helloyanis@hotmail.com>
2023-12-22 10:40:23 +0100

    Improve deeplinks

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-document.querySelector('#download-app').addEventListener('click', () => {
-    window.open('https://www.example.com');
+document.onload = () => {
+    document.querySelector('#download-app').addEventListener('click', () => {
+        window.open('https://www.example.com');
     });
+        const urlParams = new URLSearchParams(window.location.search);
+        if (urlParams.has('param')) {
+            document.querySelector('#authentication-success').classList.remove('hidden');
+        }
+    }
+    
