7768def8fa84fff113bab841a3b27a85c5c053d1
helloyanis <helloyanis@hotmail.com>
2024-03-11 11:00:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-document.onload = () => {
+document.addEventListener("DOMContentLoaded", (event) => {
     document.querySelector('#download-app').addEventListener('click', () => {
         window.open('https://www.example.com');
     });
-        const urlParams = new URLSearchParams(window.location.search);
-        if (urlParams.has('param')) {
-            document.querySelector('#authentication-success').classList.remove('hidden');
-        }
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has('code') || urlParams.has('error')) {
+        alert('Authentication successful');
+        document.querySelector('#authentication-success').classList.remove('hidden');
     }
+});
     
